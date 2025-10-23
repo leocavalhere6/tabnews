07fc813d614a22178485b06217412773d747622c
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2025-10-23 15:28:44 +0000

    add a more robust ssl configuration connect method

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   console.log(responseBody);
 
-  expect(responseBody.dependencies.database.version).toEqual("17.5");
+  expect(responseBody.dependencies.database.version).toEqual("16.10");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
