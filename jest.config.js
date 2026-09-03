8007d49e287a75e08f3100e79d3baf56e67b2781
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2026-09-03 12:53:01 +0000

    test: add code coverage setup

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,20 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({
   dir: ".",
 });
+
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
   testTimeout: 60000,
+  collectCoverageFrom: [
+    "infra/**/*.{js,ts}",
+    "models/**/*.{js,ts}",
+    "pages/api/**/*.{js,ts}",
+    "!**/*.test.{js,ts}",
+    "!infra/migrations/**",
+    "!infra/scripts/**",
+  ],
+  coverageReporters: ["text", "lcov", "html"],
+  coverageDirectory: "coverage",
 });
 
 module.exports = jestConfig;
