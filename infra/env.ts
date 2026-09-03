e0ea230e9d1fb3090ba91b22455ba3c7c7ed8dd3
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2026-09-03 01:32:39 +0000

    build(deps): setup redis service and environment configuration

--- infra/env.ts
+++ infra/env.ts
@@ -9,6 +9,8 @@ const envSchema = z.object({
   POSTGRES_USER: z.string().min(1),
   POSTGRES_DB: z.string().min(1),
   POSTGRES_PASSWORD: z.string().min(1),
+  REDIS_HOST: z.string().min(1).default("localhost"),
+  REDIS_PORT: z.coerce.number().default(6379),
 });
 
 export const env = envSchema.parse(process.env);
